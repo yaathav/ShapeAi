4598396e6491ba9e91e411e37318b52d0be11f36
yaathav <79856399+yaathav@users.noreply.github.com>
2021-06-26 13:14:24 +0530

    Add files via upload

---
+++ ShapeAi/src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>A 7-days BootCamp Where you can learn.</p>
+    </div>
+  );
+}
+
+export default Info;
